c4c315fbb7e7393614726d6f53af462850819077
bakhodirovu <bakhodirovumid1@gmail.com>
2022-01-28 19:03:01 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+window.addEventListener('DOMContentLoaded',function(){
+window.addEventListener('scroll', function(){
+    const header =this.document.querySelector('header');
+    header.classList.toggle('sticky',window.scrollY > 0)
+})
+const menuBtn =document.querySelector('.menu-btn')
+const navigation =document.querySelector('.navigation')
+const navigationItems =document.querySelectorAll('.navigation')
+
+menuBtn.addEventListener('click' ,() => {
+    menuBtn.classList.toggle('active')
+    navigation.classList.toggle('active')
+})
+
+navigationItems.forEach(navItem =>{
+    navItem.addEventListener('click', () =>{
+        menuBtn.classList.remove('active')
+        navigation.classList.remove('active')
+    })
+})
+
+})
